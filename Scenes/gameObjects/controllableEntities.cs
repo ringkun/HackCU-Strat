2da7b49d6d20dacbc29128345e2684632d6edd80
ringkun <ringkun12@gmail.com>
2020-02-22 19:45:26 -0700

    Playable Entity files

---
+++ Scenes/gameObjects/controllableEntities.cs
@@ -0,0 +1,61 @@
+using Godot;
+using System;
+using System.Security;
+
+public class controllableEntities : Sprite
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+/*Current Player Position*/
+	private Vector2 position;
+/*Number of Stone in possesion*/
+	private int stones;
+/*How many moves per turn the player has*/
+	private int walkspeed;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+/*Return Current Position*/
+	public Vector2 getCoord(){
+		return position;
+	}
+	private bool validPos(int x, int y)
+	{
+		bool b = false;
+		return b;
+	}
+
+	public bool setCoord(int x, int y)
+	{
+		if (validPos(x, y))
+		{
+			this.Position = new Vector2(x, y);
+		}
+	}
+
+	private int isWall(int x, int y)
+	{
+		int b = 1;
+		return b;
+	}
+
+	public void examine()
+	{
+		Vector2 currPos = getCoord();
+		int[] arr = {1,0,-1,0};
+		int wallCount = 0;
+		for (int i = 0; i < 4; i++)
+			wallCount = wallCount + isWall((int)currPos.x + arr[i], (int)currPos.y + arr[(i + 1) % 3]);
+		{
+		GD.Print("There are "+wallCount+" walls surrounding you.");	
+		}				
+	}
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
--- Scenes/uiElement/ItemButtonInteract.cs
+++ Scenes/uiElement/ItemButtonInteract.cs
@@ -6,27 +6,31 @@ public class ItemButtonInteract : Button
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
-
+	private Tween TweenN;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-	{		GD.Print(this.AnchorTop);
+	{
+		TweenN = this.GetParent().GetNode<Tween>("selectionTween");
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
 //  {
 //      
-//  }//onready var TweenN = get_node("Tween");
-	private float anchorReturn = 0;
-	public float anchorReveal = 20;
-	private void _on_Button_focus_entered(){
+//  }
 
-		//    TweenN.interpolate_property(self, "anchor", getpos(), anchorReveal, Tween.TRANS_BACK, Tween.EASEOUT)
+	/*private float anchorReturn = 0;
+	public float anchorReveal = -1;
+	private void _on_Button_mouse_entered(){
+		TweenN.InterpolateProperty( (Control)(this.GetParent()), "AnchorTop", ((Control)this.GetParent()).AnchorTop, anchorReveal,1, Tween.TransitionType.Back, Tween.EaseType.Out);
+		GD.Print(((Control)this.GetParent()).AnchorTop);
+		TweenN.Start();
 	}
-	private void _on_Button_focus_exited()
-	{
-		// Replace with function body.
-	}
+	private void _on_Button_mouse_exited(){
+		TweenN.InterpolateProperty( (Control)(this.GetParent()), "AnchorTop", ((Control)this.GetParent()).AnchorTop, anchorReturn,1,  Tween.TransitionType.Back, Tween.EaseType.Out);
+		GD.Print(((Control)this.GetParent()).AnchorTop);
+		TweenN.Start();
+	}*/
 
 
 }
